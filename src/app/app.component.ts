0d937dbfd54be63d24d009054bded8a407ccf770
nilu986 <nilu.986@gmail.com>
2021-04-01 11:15:25 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-@Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-})
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
+export class AppComponent implements OnInit {
+    registerForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit() {
+        this.registerForm = this.formBuilder.group({
+            userName: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            acceptTerms: [false, Validators.requiredTrue]
+        });
+    }
+
+    get formC() { 
+    return this.registerForm.controls; }
+
+    onSubmit() {
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+    }
+
+    
 }
